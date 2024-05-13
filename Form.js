1c3149b752842fcf071ce87ffb501c818fcf35d2
Prakash Baskar <147471810+Prakash-baskar@users.noreply.github.com>
2024-05-13 17:56:49 +0530

    Add files via upload
    new commit

---
+++ Form.js
@@ -0,0 +1,31 @@
+
+
+
+const Form = () => {
+  
+    
+   
+  return (
+    <form>
+      <div className='main'>
+        <label>First Name</label>
+        <input></input>
+        <label>Last Name</label>
+        <input></input>
+        <label>Age</label>
+        <input></input>
+        <label>Gender</label>
+        <input></input>
+        <label>Blood Group</label>
+        <input></input>
+        <label>Email</label>
+        <input></input>
+        <label>Phone Number</label>
+        <input></input>
+        <button>Register</button>
+      </div>
+    </form>
+  )
+}
+
+export default Form;
